5081fe81e78319a1009ca0332a11e7fd785b254f
Ethan Hofton <ethanhofton@gmail.com>
2022-01-13 14:39:44 +0000

    added buffer, context, device, and support

---
+++ include/mayCL/device.hpp
@@ -0,0 +1,19 @@
+#ifndef device_hpp
+#define device_hpp
+
+#include <OpenCL/opencl.h>
+
+namespace mayCL
+{
+    class Device
+    {
+    public:
+
+        Device();
+        ~Device();
+
+        
+    };
+}
+
+#endif
